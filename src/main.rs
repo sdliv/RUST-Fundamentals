af402d35d737adf8a96f7c247697774e1a7e692c
Sean Livingston <sean.d.livingston@gmail.com>
2017-02-26 19:08:28 -0500

    Scope

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
-    let x: i32;
+    // Scope and shadowing
+    let x: i32 = 17;
+    {
+        let y: i32 = 3;
+        println!("The value of x is {} and the value of y is {}", x, y);
+    }
+    println!("The value of x is {} and the value of y is {}", x, y); // This will not work due to y being out of scope
 
-    println!("The value of x is : {}", x);
 }
