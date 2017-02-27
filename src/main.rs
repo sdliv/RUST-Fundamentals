781f036559b14b234d71fae4b750cac121d794c9
Sean Livingston <sean.d.livingston@gmail.com>
2017-02-27 13:10:32 -0500

    Slicing syntax example.

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,13 @@
     bool: represents boolean, true or false
     char: represents a single unicode scalar value. char is 4 bytes in Rust, not just 1
 
-    NUMERIC TYPES:
-    Categories of numeric types:
-    1. signed and unsigned
-    2. fixed and variable
-    3. floating-point and integer
+    SLICES
 */
 
 fn main() {
-    // Arrays: they are immutable by default, can be made mutable, can hold type, and limits be set
-
-    let a = [1,2,3]; // a:[i32; 3]
-    let mut m = [1,2,3]; // m:[i32; 3]
-    let c = [0; 20];  // short hand initializing of an array of 20 elements, all set to 0
-    let names = ["Sean", "Nicole", "Joanna", "Adoniah"]; // names: [&str; 3]
-
-    println!("The length of array c, is: {}", c.len());
-    println!("The second name in the 'names' array is: {}", names[1]);
+    let a = [1,2,3,4,5,6,7,8,9,10];
+    let complete = &a[..]; // a slice containing all of the elements in 'a'.
+    let middle = &a[1..4]; // a slice containing elements 1 through 3, not including 4 in 'a'.
+    println!("The complete array: {}", complete[1]); // outputs 2
+    println!("The middle of the array: {}", middle[0]); // also outputs 2, because [1..4] where element 0 in this slice = 2
 }
