807f7c72429f56ba00d7419ed029b65899732ae9
Sean Livingston <sean.d.livingston@gmail.com>
2017-02-27 12:17:30 -0500

    Function Pointers

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+/*
+    FUNCTION POINTERS
+*/
+
 fn main() {
-    let x: i32 = diverges();
+    let f: fn(i32) -> i32 = plus_one; // Without type inference
+    let g = plus_one; // With type inference
+
+    let seven = f(6);
+    let eight = g(7);
+    println!("using the function f(6) = {}\nusing the function g(7) = {}", seven, eight);
+
 }
 
-
-fn diverges() -> ! {
-    panic!("This function does not return");
+fn plus_one(i: i32) -> i32 {
+    i + 1
 }
