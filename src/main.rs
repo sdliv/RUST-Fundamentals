f29a890f680ba928d2854ad6241587a75c035c76
Sean Livingston <sean.d.livingston@gmail.com>
2017-02-28 10:28:12 -0500

    Learning about comments in Rust.  Very powerful. Features Line comments, and doc comments.

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,30 @@
 /*
-    PRIMITIVE TYPES:
-    bool: represents boolean, true or false
-    char: represents a single unicode scalar value. char is 4 bytes in Rust, not just 1
-
-    TUPLES: an ordered list with fixed size.
+    COMMENTS:
+    Rust has line comments and 'doc' comments.  Doc comments have three '/' and support markdwon
 */
 
 fn main() {
-    let x = (1, "hello"); // with inference
-    let y: (i32, &str) = (2, "hello"); //  without type inference
+    let x = 5; // this is a line comment
+    println!("Adding one to {} is {}", x, add_one(x));
+}
 
-    /*
-        You can assign one tuple into another
-        if they have the same contained types and arity
-        Tuples have the same airty when they have the same
-        length.
-    */
 
-    let mut z = (1, 2); // z: (i32, i32)
-    let t = (3, 4);
+// Example of doc comment below:
 
-    z = t;
+/// Add one to the given number
+/// 
+/// # Examples
+///
+/// ```
+///
+/// let five = 5;
+///
+/// assert_eq!(6, add_one(5));
+/// # fn add_one(x: i32) -> i32 {   
+/// #     x + 1
+/// # }
+/// ```
 
-    let (r, s) = t;
-    println!("r is {}", r);
-
-    /*
-        Tuple Indexing
-    */
-
-    let tuple = (1, 2, 3);
-
-    let i = tuple.0;
-    let j = tuple.1;
-    let k = tuple.2;
-
-    println!("The value of i: {}\nThe value of j: {}\nThe value of k: {}", i, j, k);
-
-    /*
-        And somehow, FUNCTIONS CAN HAVE TYPES!
-    */
-
-    fn foo(x: i32) -> i32 { x }
-
-    let a: fn(i32) -> i32 = foo;
-    // in this case 'a' is a 'function pointer' to a function that takes an i32 and returns an i32.
+fn add_one(x: i32) -> i32 {
+    x + 1
 }
